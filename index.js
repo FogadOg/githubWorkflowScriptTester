17dea75c76b764cce162ac93dbb68ce751c8a633
FogadOg <115077292+FogadOg@users.noreply.github.com>
2023-11-22 12:35:40 +0100

    test

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 const args = process.argv; // This gets the command line arguments, excluding node and the script file
 
-console.log('Arguments passed:', args);
 
 function replaceEnvAndOrg(org, env, fileContent){
-    console.log("org, env: ",org, env)
     fileContent=String(fileContent)
 
     substitutedText=fileContent.replace("{env}",env)
     substitutedText=substitutedText.replace("{org}",org)
 
-    console.log(substitutedText);
+    console.log("substitutedText: ",substitutedText);
 
     return substitutedText
 }
-
-console.log("args[-2], args[-1]: ",args[2], args[3]);
 
 replaceEnvAndOrg(args[2], args[3],"This gets {org} the command line arguments, {env} excluding node and the script file")
