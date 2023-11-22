4e1b9ef73b625426a793dfc2e044484e646446e9
FogadOg <115077292+FogadOg@users.noreply.github.com>
2023-11-22 12:14:39 +0100

    test

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-const org = process.argv[0];
-const env = process.argv[1];
-const fileContent = process.argv[2];
+const org = process.argv;
 
 console.log('Arguments passed org:', org);
-console.log('Arguments passed env:', env);
-console.log('Arguments passed:', fileContent);
 
 function replaceEnvAndOrg(org, env, fileContent){
     fileContent=String(fileContent)
