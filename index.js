e0d6f8c70ab16816717ebd86c123c15cc73d8f1a
FogadOg <fogadm06@gmail.com>
2023-11-26 13:36:18 +0100

    test

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const org = process.env.INPUT_ORG;
 const env = process.env.INPUT_ENV;
 const manifest = process.env.INPUT_MANIFEST;
 
-console.log("manifest: ",manifest);
-console.log("env: ",env);
-console.log("org: ",org);
 
 function replaceEnvAndOrg(org, env, fileContent){
     fileContent=String(fileContent)
@@ -12,7 +9,6 @@ function replaceEnvAndOrg(org, env, fileContent){
     substitutedText=fileContent.replace("{env}",env)
     substitutedText=substitutedText.replace("{org}",org)
 
-    console.log(substitutedText);
 
     return substitutedText
 }
