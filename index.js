6b12056696fd078238d9bd480808ed6f29acf63a
FogadOg <115077292+FogadOg@users.noreply.github.com>
2023-11-29 10:53:50 +0100

    testing if the replacer returns data

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
+const org = process.env.INPUT_ORG;
+const env = process.env.INPUT_ENV;
+const manifest = process.env.INPUT_MANIFEST;
+
 const fs = require("fs");
 
-function replaceEnvAndOrg(org, env, manifest) {
-  return new Promise((resolve, reject) => {
+function replaceEnvAndOrg(org, env, manifest){
+
     fs.readFile("config.yaml", (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        let text = data.toString();
-        let substitutedText = text.replace("{env}", env);
-        substitutedText = substitutedText.replace("{org}", org);
-        resolve(substitutedText);
-      }
-    });
-  });
+        if (err) throw err;
+        text=data.toString();
+
+        substitutedText=text.replace("{env}",env)
+        substitutedText=substitutedText.replace("{org}",org)
+        console.log(substitutedText);
+        return substitutedText
+      });
+
+
+    
+
 }
 
-replaceEnvAndOrg(process.env.INPUT_ORG, process.env.INPUT_ENV, process.env.INPUT_MANIFEST)
-  .then((replacedText) => {
-    console.log(replacedText); // Output the replaced text
-    process.stdout.write(`::set-output name=replacedData::${replacedText}`); // Set output for the action
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-    process.exit(1);
-  });
+replaceEnvAndOrg(org, env, manifest)
+
+
