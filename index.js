89efd485889690bae4ae0fb2a69e0d9b33461ace
FogadOg <115077292+FogadOg@users.noreply.github.com>
2023-11-29 13:46:18 +0100

    testing if the replacer returns data

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const fs = require("fs");
 
-function replaceEnvAndOrg(org, env, manifest) {
+async function replaceEnvAndOrg(org, env, manifest) {
     return new Promise((resolve, reject) => {
-        fs.readFile("config.yaml", (err, data) => {
+        fs.readFile(manifest, (err, data) => {
             if (err) reject(err);
             let text = data.toString();
 
             let substitutedText = text.replace("{env}", env);
             substitutedText = substitutedText.replace("{org}", org);
-            console.log(substitutedText);
             
-            resolve(substitutedText); // Resolve the Promise with the substituted text
+            resolve(substitutedText); // Resolve with the substituted text
         });
     });
 }
