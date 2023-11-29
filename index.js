5d1bacb176eeeedebbed10c72b858cfb730be95b
FogadOg <115077292+FogadOg@users.noreply.github.com>
2023-11-29 10:04:42 +0100

    test

--- index.js
+++ index.js
@@ -2,19 +2,25 @@ const org = process.env.INPUT_ORG;
 const env = process.env.INPUT_ENV;
 const manifest = process.env.INPUT_MANIFEST;
 
+const fs = require("fs");
+
 console.log("manifest: ",manifest)
 ///home/runner/work/_temp/baked-template-1701248034373.yaml
 function replaceEnvAndOrg(org, env, manifest){
-    fetch(manifest)
-    .then((res) => res.text())
-    .then((text) => {
-    
+
+    fs.readFile(manifest, (err, data) => {
+        if (err) throw err;
+        text=data.toString();
+
         substitutedText=text.replace("{env}",env)
         substitutedText=substitutedText.replace("{org}",org)
     
         return substitutedText
-    })
-    .catch((e) => console.error(e));
+      });
+
+
+    
+
 }
 
 replaceEnvAndOrg(org, env, manifest)
