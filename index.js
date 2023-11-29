a55ae1257b545f53aff0908a64f85ae64aeb74fa
FogadOg <115077292+FogadOg@users.noreply.github.com>
2023-11-29 14:52:53 +0100

    testing if the replacer returns data

--- index.js
+++ index.js
@@ -14,12 +14,10 @@ function replaceEnvAndOrg(org, env, manifest){
         substitutedText=text.replace("{env}",env)
                             .replace("{org}",org)
 
-        console.log("substitutedText: ",substitutedText);
 
         console.log(`::set-output name=result::${substitutedText.replace(/\n/g, '%0A')}`);
 
         
-        return substitutedText
       });
 
 
